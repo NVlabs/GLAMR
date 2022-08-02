698a9bb44ee68d8a7307db96ea7b77affa0a1e4b
Ye Yuan <yey@nvidia.com>
2022-08-02 06:00:45 -0700

    minor fix

--- install.sh
+++ install.sh
@@ -1,8 +1,2 @@
 # apt
 sudo apt install libgl1-mesa-glx xvfb
-
-# mesa
-sudo apt update
-sudo wget https://github.com/mmatl/travis_debs/raw/master/xenial/mesa_18.3.3-0.deb
-sudo dpkg -i ./mesa_18.3.3-0.deb || true
-sudo apt install -f
