dcb2ea14695396e2354b9aabf7eaf506258e45c9
Ye Yuan <yey@nvidia.com>
2022-08-08 08:15:59 -0700

    fix hybrik demo

--- pose_est/hybrik_demo/demo.py
+++ pose_est/hybrik_demo/demo.py
@@ -68,11 +68,13 @@ opt = parser.parse_args()
 cfg_file = 'configs/256x192_adam_lr1e-3-res34_smpl_3d_cam_2x_mix.yaml'
 cfg = update_config(cfg_file)
 
+bbox_3d_shape = getattr(cfg.MODEL, 'BBOX_3D_SHAPE', (2000, 2000, 2000))
+bbox_3d_shape = [item*1e-3 for item in bbox_3d_shape]
 dummpy_set = edict({
     'joint_pairs_17': None,
     'joint_pairs_24': None,
     'joint_pairs_29': None,
-    'bbox_3d_shape': (2.2, 2.2, 2.2)
+    'bbox_3d_shape': bbox_3d_shape
 })
 
 transformation = SimpleTransform3DSMPL(
@@ -82,7 +84,7 @@ transformation = SimpleTransform3DSMPL(
     input_size=cfg.MODEL.IMAGE_SIZE,
     output_size=cfg.MODEL.HEATMAP_SIZE,
     depth_dim=cfg.MODEL.EXTRA.DEPTH_DIM,
-    bbox_3d_shape=(2.2, 2,2, 2.2),
+    bbox_3d_shape=bbox_3d_shape,
     rot=cfg.DATASET.ROT_FACTOR, sigma=cfg.MODEL.EXTRA.SIGMA,
     train=False, add_dpg=False,
     loss_type=cfg.LOSS['TYPE'])
@@ -166,10 +168,6 @@ for img_path in tqdm(img_path_list):
     transl = pose_output.transl.detach().cpu().numpy().squeeze()
     transl[2] = transl[2] * 256 / bbox_xywh[2]
 
-    new_princpt = np.array([image.shape[1], image.shape[0]]) * 0.5
-    transl[:2] += (np.array(princpt) - new_princpt) * transl[2] / np.array(focal) 
-    princpt = new_princpt
-
     image_vis = vis_smpl_3d(
         pose_output, image, cam_root=transl,
         f=focal, c=princpt, renderer=renderer)
@@ -193,6 +191,10 @@ for img_path in tqdm(img_path_list):
     # cv2.imwrite(res_path, bbox_img)
 
     bboxes.append(np.array(bbox_xywh))
+
+    new_princpt = np.array([image.shape[1], image.shape[0]]) * 0.5
+    transl[:2] += (np.array(princpt) - new_princpt) * transl[2] / np.array(focal) 
+    princpt = new_princpt
 
     # save to dict
     K = np.eye(3)
